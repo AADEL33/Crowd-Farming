97857f441596db930f9a776027de6c8620aa9528
AADEL Abdelhalim <adilabdelhalim3@gmail.com>
2023-01-15 14:59:33 +0100

    first commit

---
+++ src/main/java/com/example/crowdfarming/CrowdFarmingApplication.java
@@ -0,0 +1,13 @@
+package com.example.crowdfarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdFarmingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdFarmingApplication.class, args);
+    }
+
+}
